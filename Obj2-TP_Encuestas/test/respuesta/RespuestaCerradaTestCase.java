3c62fb3118c6a97fe2086aabb4417a9f032266f1
Enzo <EnzoGl33@hotmail.com>
2019-06-07 10:46:41 -0300

    Respuesta con workflowTest

--- Obj2-TP_Encuestas/test/respuesta/RespuestaCerradaTestCase.java
+++ Obj2-TP_Encuestas/test/respuesta/RespuestaCerradaTestCase.java
@@ -4,6 +4,8 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import pregunta.PreguntaDeSeleccion;
+import workflow.Workflow;
+
 import static org.mockito.Mockito.*;
 
 class RespuestaCerradaTestCase {
@@ -12,13 +14,15 @@ class RespuestaCerradaTestCase {
     private PreguntaDeSeleccion preguntaAResponderSimple;
     private RespuestaCerrada respuestaAMultiple;
     private RespuestaCerrada respuestaASimple;
-	
+	private Workflow workflow;
+    
 	@BeforeEach
 	public void setUp() {
+		this.workflow = mock(Workflow.class);
 		this.preguntaAResponderMultiple = mock(PreguntaDeSeleccion.class);
 		this.preguntaAResponderSimple = mock(PreguntaDeSeleccion.class);
-		this.respuestaAMultiple = new RespuestaCerrada("25",preguntaAResponderMultiple);
-		this.respuestaASimple = new RespuestaCerrada("35",preguntaAResponderSimple);
+		this.respuestaAMultiple = new RespuestaCerrada("25",preguntaAResponderMultiple,workflow);
+		this.respuestaASimple = new RespuestaCerrada("35",preguntaAResponderSimple,workflow);
 	}
 	
 	@Test
@@ -34,19 +38,20 @@ class RespuestaCerradaTestCase {
 	}
 	
 	@Test
-	void testUnaRespuestaCerradaSeRespondeConUnTextoDeRespuestaSiEsPosibleResponder() {
+	void testUnaRespuestaCerradaSeRespondeConUnTextoDeRespuestaSiEsPosibleResponderYCambiaLaSiguientePreguntaDeSuWorkflow() {
 		when(preguntaAResponderMultiple.comprobarSiEsRespuestaPosible(respuestaAMultiple)).thenReturn(true);
-		respuestaAMultiple.responder("26");
+		respuestaAMultiple.responder(respuestaAMultiple.getTextoRespuesta());
 		
-		assertEquals("26",respuestaAMultiple.getTextoRespuesta());
+		assertEquals("25",respuestaAMultiple.getTextoRespuesta());
+		verify(workflow,times(1)).siguiente();
 	}
 	
 	@Test
-	void testUnaRespuestaCerradaNoSeRespondeSiNoEsPosibleLaRespuesta() {
+	void testUnaRespuestaCerradaNoSeRespondeSiNoEsPosibleLaRespuestaYNoCambiaSuWorkflow() {
 		when(preguntaAResponderSimple.comprobarSiEsRespuestaPosible(respuestaASimple)).thenReturn(false);
 		respuestaASimple.responder("Es Nueva");
-		
 		assertEquals("35",respuestaASimple.getTextoRespuesta());
+		verifyZeroInteractions(workflow);
 	}
 	
 }
--- Obj2-TP_Encuestas/test/respuesta/RespuestaTestCase.java
+++ Obj2-TP_Encuestas/test/respuesta/RespuestaTestCase.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import pregunta.*;
+import workflow.Workflow;
 
 import static org.mockito.Mockito.*;
 
@@ -15,12 +16,14 @@ class RespuestaTestCase {
 	private Respuesta respuestaLibre, respuestaCerrada;
 	private PreguntaAbierta preguntaAResponderLibre;
 	private PreguntaDeSeleccion preguntaAResponderCerrada;
+	private Workflow workflow;
 	
 	@BeforeEach
 	public void setUp() {
+		workflow = mock(Workflow.class);
 		preguntaAResponderLibre = mock(PreguntaAbierta.class);
 		preguntaAResponderCerrada = mock(PreguntaDeSeleccion.class);
-		respuestaCerrada = new RespuestaCerrada("Mas de 30",preguntaAResponderCerrada);
+		respuestaCerrada = new RespuestaCerrada("Mas de 30",preguntaAResponderCerrada,workflow);
 		respuestaLibre = new RespuestaLibre(preguntaAResponderLibre);
 	}
 	
