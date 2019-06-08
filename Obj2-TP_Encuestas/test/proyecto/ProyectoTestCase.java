78f1dbe9efb4aef185a6440edb125c74f3affc6b
ignacio-r <ignaciorobledounq@gmail.com>
2019-06-08 16:30:50 -0300

    test de proyecto

--- Obj2-TP_Encuestas/test/proyecto/ProyectoTestCase.java
+++ Obj2-TP_Encuestas/test/proyecto/ProyectoTestCase.java
@@ -1,25 +1,26 @@
 package proyecto;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import encuesta.Encuesta;
+import static org.mockito.Mockito.*;
 
-import static org.mockito.Mockito.*;  
+import java.util.ArrayList;  
 
 class testProyecto {
 
 	private Proyecto proyecto1;
-	private Encuesta encuesta1;
+	private Encuesta encuesta1, encuesta2;
+	private ArrayList<Encuesta> encuestas;
 	
 	@BeforeEach  
 	void setUp()  { 
-		
+		encuestas = new ArrayList<Encuesta>();
 		encuesta1 = mock(Encuesta.class);
+		encuesta2 = mock(Encuesta.class);
 		proyecto1 = new Proyecto("Proyecto");
-		proyecto1.agregarEncuesta(encuesta1);
+		encuestas.add(encuesta1); encuestas.add(encuesta2);
 	}
 
 	@Test
@@ -28,8 +29,12 @@ class testProyecto {
 	}
 
 	@Test
-	void testAgregarEncuesta() {
-		assertEquals(1,proyecto1.cantidadDeEncuestas());
+	void poseeUnConjuntoDeEncuestas() {
+		proyecto1.agregarEncuesta(encuesta1);
+		proyecto1.agregarEncuesta(encuesta2);
+		
+		assertEquals(encuestas, proyecto1.getEncuestas());
+		assertEquals(2,proyecto1.cantidadDeEncuestas());
 	}
 	
 }	
