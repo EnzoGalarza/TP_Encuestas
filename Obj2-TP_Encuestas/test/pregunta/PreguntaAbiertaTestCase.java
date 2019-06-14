8f248c82ccee234169a9bcdfebd7d11dfb37e886
Enzo <EnzoGl33@hotmail.com>
2019-06-14 20:00:50 -0300

    Pregunta conSiguientePregunta

--- Obj2-TP_Encuestas/test/pregunta/PreguntaAbiertaTestCase.java
+++ Obj2-TP_Encuestas/test/pregunta/PreguntaAbiertaTestCase.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import respuesta.Respuesta;
 import respuesta.RespuestaLibre;
 
 import static org.mockito.Mockito.*;
@@ -11,15 +12,25 @@ import static org.mockito.Mockito.*;
 class PreguntaAbiertaTestCase {
 
 	private PreguntaAbierta pregunta; 
+	private Pregunta preguntaSiguiente;
+	private Respuesta respuesta;
 	
 	@BeforeEach
 	public void setUp() {
 		this.pregunta = new PreguntaAbierta("Es de color verde");
+		this.preguntaSiguiente = mock(Pregunta.class);
+		this.respuesta = mock(Respuesta.class);
 	}
 	
 	@Test
 	void testUnaPreguntaAbiertaTieneUnTextoDePregunta() {
 		assertEquals("Es de color verde?",this.pregunta.getTextoPregunta());
+	}
+	
+	@Test
+	void testUnaPreguntaAbiertaPuedeSetearSuSiguientePregunta() {
+		this.pregunta.setSiguientePregunta(preguntaSiguiente);
+		assertEquals(preguntaSiguiente,pregunta.getSiguientePregunta(respuesta));
 	}
 	
 }
