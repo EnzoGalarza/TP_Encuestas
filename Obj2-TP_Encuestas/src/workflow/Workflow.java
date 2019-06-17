4706a20452e1609215080a9909adc55b784b74ce
Enzo <EnzoGl33@hotmail.com>
2019-06-16 21:56:32 -0300

    Workflow conPreguntaActual

--- Obj2-TP_Encuestas/src/workflow/Workflow.java
+++ Obj2-TP_Encuestas/src/workflow/Workflow.java
@@ -9,18 +9,11 @@ import respuesta.Respuesta;
 
 public class Workflow {
 
-	private List<Pregunta> preguntas;
-	private Integer posicionPreguntaActual;
-	private Integer posicionPreguntaAnterior;
 	private ArchivoDeRespuestas archivo;
 	private Pregunta preguntaActual;
 	
-	public Workflow(ArchivoDeRespuestas archivo,Pregunta pregunta) {
-		this.posicionPreguntaActual = 0;
-		this.posicionPreguntaAnterior = -1;
-		this.preguntas = new ArrayList<Pregunta>();
-		this.preguntas.add(pregunta);
-		this.preguntaActual = pregunta;
+	public Workflow(ArchivoDeRespuestas archivo,Pregunta preguntaInicial) {
+		this.preguntaActual = preguntaInicial;
 		this.archivo = archivo;
 	}
 
@@ -29,46 +22,19 @@ public class Workflow {
 		return this.preguntaActual;
 	}
 
-
-	public Integer posicionPreguntaActual() {
-		return this.posicionPreguntaActual;
-	}
-
-
-	public void agregarPregunta(Pregunta nuevaPregunta) {
-		this.preguntas.add(nuevaPregunta);
-	}
-
-
-	public void siguiente() {
+	public void siguiente(Respuesta r) {
 		if(this.continua()) {
-			this.posicionPreguntaActual++;
-			this.posicionPreguntaAnterior++;
-			this.preguntaActual = this.preguntas.get(posicionPreguntaActual);
+			this.preguntaActual = this.preguntaActual.getSiguientePregunta(r);
 		}
 	}
 
 	public Boolean continua() {
 		//Dice si quedan preguntas por responder.
-		return this.posicionPreguntaActual+1 < this.preguntas.size();
+		return true;
 	}
 
 	public void anterior() {
-		if(this.posicionPreguntaAnterior > 0) {
-			this.preguntaActual = this.preguntas.get(posicionPreguntaAnterior);
-			this.posicionPreguntaActual--;
-			this.posicionPreguntaAnterior--;
-		}
-	}
-	
-	public void responder(Respuesta respuesta) {
-		//Aca cambie esto del responder para la siguiente pregunta... igual hay que preguntar
-		// en clase por que seguimos con los problemas de tipo 
-		this.archivo.guardar(this.getPregunta(),respuesta);
-		if(this.getPregunta().esPreguntaSeleccionSimple()) {
-			this.posicionPreguntaAnterior = this.posicionPreguntaActual;
-			this.posicionPreguntaActual = this.preguntas.indexOf(respuesta.getSiguientePregunta()); 
-		}
+		
 	}
 	
 }
