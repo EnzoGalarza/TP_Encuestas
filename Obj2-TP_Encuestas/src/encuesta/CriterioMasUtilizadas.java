47365cfae7725fcb35303ebe3516ae880eee4e74
ignacio-r <ignaciorobledounq@gmail.com>
2019-06-29 15:47:38 -0300

    criterios de busqueda

---
+++ Obj2-TP_Encuestas/src/encuesta/CriterioMasUtilizadas.java
@@ -0,0 +1,33 @@
+package encuesta;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class CriterioMasUtilizadas extends CriterioDeBusqueda{
+	
+	private Integer cantMaximaPosiible = 25;
+
+	@Override
+	public List<Encuesta> filtrarPorCriterio(List<Encuesta> encuestas) {
+		Collections.sort(encuestas, new Comparator<Encuesta>(){		
+			public int compare(Encuesta e1, Encuesta e2) {
+				return Integer.valueOf(e1.cantidadDeRespuestasCompletas().compareTo(e2.cantidadDeRespuestasCompletas()));
+			}	
+		});	
+		if(encuestas.size() < cantMaximaPosiible) {
+			cantMaximaPosiible =  encuestas.size();
+		}
+		return encuestas.subList(0, cantMaximaPosiible);
+	}
+
+
+
+
+
+
+
+
+
+}
+	
---
+++ Obj2-TP_Encuestas/src/encuesta/CriterioPorProyecto.java
@@ -0,0 +1,38 @@
+package encuesta;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import proyecto.Proyecto;
+
+public class CriterioPorProyecto extends CriterioDeBusqueda{
+	
+	List<Proyecto> proyectos;
+	
+	public CriterioPorProyecto(List<Proyecto> proyectos) {
+		this.proyectos = proyectos; 
+	}
+
+	@Override
+	public List<Encuesta> filtrarPorCriterio(List<Encuesta> encuestas) {
+		List<Encuesta> encuestasOrdenadas = new ArrayList<>();
+		
+		Collections.sort(proyectos, new Comparator<Proyecto>()
+		{		
+			public int compare(Proyecto e1, Proyecto e2) {
+				return Integer.valueOf(e1.descripcion().compareTo(e2.descripcion()));
+			}	
+		});	
+		
+		for(Proyecto p: proyectos) {
+			encuestasOrdenadas.addAll(p.getEncuestas());
+		}
+		
+		
+		return encuestasOrdenadas;
+	}
+
+
+}
