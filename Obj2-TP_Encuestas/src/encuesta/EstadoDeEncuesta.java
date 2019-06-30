f73aff5b3df70717e406b17c9db9ea6c8d684e7d
Enzo <EnzoGl33@hotmail.com>
2019-06-30 17:47:56 -0300

    Encuesta con estados

--- Obj2-TP_Encuestas/src/encuesta/EstadoDeEncuesta.java
+++ Obj2-TP_Encuestas/src/encuesta/EstadoDeEncuesta.java
@@ -3,20 +3,46 @@ package encuesta;
 import java.util.Arrays;
 import java.util.List;
 
+import pregunta.Pregunta;
 import respuesta.Respuesta;
 
 public abstract class EstadoDeEncuesta {
 
-    private static final List<EstadoDeEncuesta> ESTADOS =
+	protected Encuesta encuesta;
+	
+    /*private static final List<EstadoDeEncuesta> ESTADOS =
     	Arrays.asList(new EstadoDeEncuestaDisponible(), new EstadoDeEncuestaCerrada());
 
     public static EstadoDeEncuesta estadoPara(Encuesta unaEncuesta) {
         return ESTADOS.stream().filter(state -> state.canHandle(unaEncuesta)).findFirst().get();
     }
-    
-    protected abstract void agregarRespuestaPara(Encuesta unaEncuesta, Respuesta unaRespuesta);
 
-    protected abstract Boolean canHandle(Encuesta unaEncuesta);
+    protected abstract Boolean canHandle(Encuesta unaEncuesta);*/
+    
+    public  void responder(Respuesta r) {
+    	//Nothing.
+    }
+    
+    public  void finalizarEdicion() {
+    	//Nothing
+    }
+    
+    public  void cerrarEncuesta() {
+    	//Nothing
+    }
+    
+    public  void guardarCambios() {
+    	//Nothing
+    }
+    
+    public  void setPregunta(Pregunta p) {
+    	//Nothing
+    }
+
+	public Boolean esDisponible() {
+		// TODO Auto-generated method stub
+		return false;
+	}
     
 }
 
