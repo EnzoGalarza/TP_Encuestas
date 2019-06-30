2ca4973d01b770232ac5e97e6d78c2a579cf4dca
Enzo <EnzoGl33@hotmail.com>
2019-06-30 17:51:24 -0300

    Encuesta conEstados

--- Obj2-TP_Encuestas/src/encuesta/EstadoDeEncuestaDisponible.java
+++ Obj2-TP_Encuestas/src/encuesta/EstadoDeEncuestaDisponible.java
@@ -4,16 +4,43 @@ import respuesta.Respuesta;
 
 public class EstadoDeEncuestaDisponible extends EstadoDeEncuesta{
 
+	public EstadoDeEncuestaDisponible(Encuesta encuesta) {
+		this.encuesta = encuesta;
+	}
+    
+	/*
 	@Override
 	protected Boolean canHandle(Encuesta unaEncuesta) {
 		return unaEncuesta.disponible() && !unaEncuesta.finalizada();
 	}
 	
-	public void agregarRespuestaPara(Encuesta unaEncuesta, Respuesta unaRespuesta) {
+	public void agregarPreguntaPara(Encuesta unaEncuesta) {
 		unaEncuesta.encapsulador().agregarRespuestaRealizada(unaEncuesta.getPreguntaActual(),unaRespuesta);
 		unaEncuesta.encapsulador().nuevaSesion();
 		unaEncuesta.actualizarCantidadDeRespuestasEsperada();
+	}*/
+	
+	@Override
+	public void responder(Respuesta r) {
+		this.encuesta.encapsulador().agregarRespuestaRealizada(this.encuesta.getPreguntaActual(), r);
+		this.encuesta.workflow().siguiente(r);
 	}
 	
+	@Override
+	public void guardarCambios() { 
+		 this.encuesta.encapsulador().nuevaSesion();
+		 this.encuesta.actualizarCantidadDeRespuestasEsperada();
+	}
+	
+	@Override
+	public void cerrarEncuesta() {
+		EstadoDeEncuesta nuevoEstado = new EstadoDeEncuestaCerrada(this.encuesta);
+		this.encuesta.setEstado(nuevoEstado);
+	}
+	
+	@Override
+	public Boolean esDisponible() {
+		return true;
+	}
 
 }
